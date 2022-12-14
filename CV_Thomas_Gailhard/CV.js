4178b19e5ad9dbd869156643c5a48f6e9a23a940
Thomas39 <118270371+Thomas39@users.noreply.github.com>
2022-12-14 17:17:39 +0100

    Add files via upload

---
+++ CV_Thomas_Gailhard/CV.js
@@ -0,0 +1,28 @@
+function button1()
+{
+    $('#mon_experience').show();
+    $('#mes_competences').hide();
+    $('#ma_formation').hide();
+}
+
+function button2()
+{
+    $('#mon_experience').hide();
+    $('#mes_competences').show();
+    $('#ma_formation').hide();
+}
+
+function button3()
+{
+    $('#mon_experience').hide();
+    $('#mes_competences').hide();
+    $('#ma_formation').show();
+}
+
+function button4()
+{
+    $('#mon_experience').show();
+    $('#mes_competences').show();
+    $('#ma_formation').show();
+}
+
